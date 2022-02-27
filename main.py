987f3dbdae7410a242ebbfa577763e1e5ce9e3ce
Sergey Yevtushok <ggazila@gmail.com>
2022-02-27 19:48:40 +0200

    Small refactoring and added exceptions

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import asyncio
+import datetime
 import random
 
 from telethon import TelegramClient
+from telethon import errors
 from telethon import functions, types
 import questionary
 
@@ -9,8 +11,6 @@ from report_text import generate_text
 
 api_id = int(questionary.password('Api ID:').ask())
 api_hash = questionary.password('Api hash:').ask()
-
-
 
 client = TelegramClient('session_new', api_id, api_hash)
 client.start()
@@ -24,12 +24,12 @@ async def main():
     telegram_list = open('telegram_db', 'r').readlines()
     random.shuffle(telegram_list)
 
-    for (i,telegram_channel) in enumerate(telegram_list[:number_of_channels_rep]):
+    for (i, telegram_channel) in enumerate(telegram_list[:number_of_channels_rep]):
         if "https://" in telegram_channel:
             telegram_channel = telegram_channel.split('/')[-1]
         elif '@' in telegram_channel:
             telegram_channel = telegram_channel[1:]
-        print(i+1, telegram_channel.strip())
+        print(i + 1, telegram_channel.strip())
         try:
             result = await client(functions.account.ReportPeerRequest(
                 peer=telegram_channel,
@@ -39,8 +39,17 @@ async def main():
             print(result)
         except ValueError:
             print("Channel not found")
+        except errors.UsernameInvalidError:
+            print("Nobody is using this username, or the username is unacceptable")
+        except errors.FloodWaitError as e:
+            seconds_left = e.seconds
+            while seconds_left > 0:
+                print("Flood wait error. Waiting for ", str(datetime.timedelta(seconds=seconds_left)))
+                seconds_left -= 60
+                await asyncio.sleep(60)
+
         await asyncio.sleep(10 + 2 * random.random())
+
+
 with client:
-
     client.loop.run_until_complete(main())
-
