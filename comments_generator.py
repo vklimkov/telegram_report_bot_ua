5c078ad187818a688e3445c6f06e9df7cce2eb8a
vklimkov <vklimkov@ueff1b1bb6afc5b.ant.amazon.com>
2022-03-02 20:40:31 +0100

    comments_generator.py: add method to generate media comments

--- comments_generator.py
+++ comments_generator.py
@@ -1,11 +1,21 @@
 
+import glob
+import json
 import random
 
 
 class CommentsGenerator:
-    def __init__(self, utterances_path="comments/text_comments.txt"):
+    def __init__(self, utterances_path="comments/text_comments.txt", json_dir="comments/jsons"):
         with open(utterances_path, 'r') as fp:
             self._utterances = [x.strip() for x in fp.readlines()]
+        self._jsons = list(glob.glob(json_dir))
 
     def get_random_text_comment(self):
         return random.choice(self._utterances)
+
+    def get_random_media_comment(self):
+        path = random.choice(self._jsons)
+        with open(path, 'r') as fp:
+            res = json.load(fp)
+        return res
+
