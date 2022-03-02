1f4749f5ee2676cba52af52ef0de48275102bc11
vklimkov <vklimkov@ueff1b1bb6afc5b.ant.amazon.com>
2022-03-02 23:33:02 +0100

    comments_generator.py: hotfix for media comments reading

--- comments_generator.py
+++ comments_generator.py
@@ -1,4 +1,5 @@
 
+import os
 import glob
 import json
 import random
@@ -8,7 +9,7 @@ class CommentsGenerator:
     def __init__(self, utterances_path="comments/text_comments.txt", json_dir="comments/jsons"):
         with open(utterances_path, 'r') as fp:
             self._utterances = [x.strip() for x in fp.readlines()]
-        self._jsons = list(glob.glob(json_dir))
+        self._jsons = list(glob.glob(os.path.join(json_dir, "*.json")))
 
     def get_random_text_comment(self):
         return random.choice(self._utterances)
